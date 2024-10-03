54cd039f949a09a37007a8ec6cc8889868d78ad3
Nic0ro <willarossi1@gmail.com>
2024-10-03 15:21:27 -0400

    setting up collect and media page

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ def nav_items_inject():
         {'name':'home','url':'/'},
         {'name':'socials','url':'/socials'},
         {'name':'travel','url':'/travel'},
-        {'name':'collections','url':'/collect'}
+        {'name':'collection','url':'/collect'},
+        {'name':'media','url':'/media'}
     ]
     return dict(nav_items=nav_items)
 
@@ -24,13 +25,44 @@ def socials():
 def travel():
     return render_template('travel.html')
 
-app.route('/collect')
+@app.route('/media')
+def media():
+    tv_shows=[]
+    with open('tv_show.txt','r') as file:
+        x=file.read()
+        x.replace('\n','<br><br>')
+        tv_shows.append(x)
+        print(tv_shows)
+
+    with open('movies.txt','r') as file:
+        file_content=file.read()
+
+    file_content.replace('\n','<br><br>')
+    print(file_content)
+
+    return render_template('media.html',content=file_content,tvcontent=tv_shows)
+
+@app.route('/collect')
 def collect():
     cards=[
         {'name':'jeweled lotus','set':'commander'}
     ]
 
     return render_template('collect.html',cards=cards)
+
+@app.route('/sitemap')
+def sitemap():
+
+    sites=[
+        {'name':'home','url':'/'},
+        {'name':'socials','url':'/socials'},
+        {'name':'travel','url':'/travel'},
+        {'name':'collection','url':'/collect'},
+        {'name':'media','url':'/media'},
+        {'name':'site map','url':'/sitemap'}
+    ]
+
+    return render_template("site_map.html",sites=sites)
 
 if __name__ == "__main__":
     app.run(debug=True)
