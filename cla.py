18a5260f9306faf88ac6382ffd651d5d028906d9
Nic0ro <willarossi1@gmail.com>
2024-10-08 22:51:32 -0400

    create class for collections

--- cla.py
+++ cla.py
@@ -5,7 +5,7 @@ import csv
 
 cla=Flask(__name__)
 
-class Home(View):
+class Basic(View):
     def __init__(self,temp_name,title,page_title,sites):
         self.temp_name=temp_name
         self.title=title
@@ -19,9 +19,41 @@ class Home(View):
         ]
         self.sites=sites
 
-    def dispatch_request(self):
-        return render_template(self.temp_name,title=self.title,page_title=self.page_title,nav_items=self.nav_items,sites=self.sites)
+    def csv_reader(self,file_name,list):
+        with open(f'static/info/{file_name}',mode='r') as file:
+            csvFile=csv.DictReader(file)
+            for line in csvFile:
+                list.append(line)
+        return list
 
+    def dispatch_request(self):
+        return render_template(
+            self.temp_name,
+            title=self.title,
+            page_title=self.page_title,
+            nav_items=self.nav_items,
+            sites=self.sites
+        )
+
+class Collect(Basic):
+                 
+    def dispatch_request(self):
+        
+        self.cards=[]
+        self.maps=[]
+            
+        self.cards=Basic.csv_reader(self,'mtg_cards.csv',self.cards)
+        self.maps=Basic.csv_reader(self,'maps.csv',self.maps)
+
+        return render_template(
+        self.temp_name,
+        title=self.title,
+        page_title=self.page_title,
+        nav_items=self.nav_items,
+        sites=self.sites,
+        cards=self.cards,
+        maps=self.maps
+        )
 templates=[]
 
 with open('template_info.csv', mode ='r')as file:
@@ -35,7 +67,10 @@ for i in templates:
      sites.append({'name':i['name'],'url':i['route']})
 
 for temp in templates:
-     cla.add_url_rule(temp['route'],view_func=Home.as_view(temp['name'],temp['template'],temp['name'],temp['page_title'],sites))
+     if temp['type']=='collect':
+        cla.add_url_rule(temp['route'],view_func=Collect.as_view(temp['name'],temp['template'],temp['name'],temp['page_title'],sites))
+else:
+    cla.add_url_rule(temp['route'],view_func=Collect.as_view(temp['name'],temp['template'],temp['name'],temp['page_title'],sites))
 
 if __name__ == '__main__':
     cla.run(debug=True)
