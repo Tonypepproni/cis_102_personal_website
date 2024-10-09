49f6e25bbb1cbd4b7a77207affff492f1a277fa3
Nic0ro <willarossi1@gmail.com>
2024-10-08 21:28:49 -0400

    made templates run through classes

--- cla.py
+++ cla.py
@@ -6,12 +6,20 @@ import csv
 cla=Flask(__name__)
 
 class Home(View):
-    def __init__(self,temp_name):
+    def __init__(self,temp_name,title,page_title):
         self.temp_name=temp_name
+        self.title=title
+        self.page_title=page_title
+        self.nav_items=[
+            {'name':'home','url':'/'},
+            {'name':'socials','url':'/socials'},
+            {'name':'travel','url':'/travel'},
+            {'name':'collection','url':'/collect'},
+            {'name':'media','url':'/media'}
+        ]
 
     def dispatch_request(self):
-        return render_template(self.temp_name)
-
+        return render_template(self.temp_name,title=self.title,page_title=self.page_title,nav_items=self.nav_items)
 
 templates=[]
 
@@ -21,7 +29,7 @@ with open('template_info.csv', mode ='r')as file:
             templates.append(line)
 
 for temp in templates:
-     cla.add_url_rule(temp['route'],view_func=Home.as_view(temp['name'],temp['template']))
+     cla.add_url_rule(temp['route'],view_func=Home.as_view(temp['name'],temp['template'],temp['name'],temp['name']))
 
 if __name__ == '__main__':
     cla.run(debug=True)
