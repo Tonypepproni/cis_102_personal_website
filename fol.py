598c2cf5ae4b0daa9dcb84e10a9a9ff9ee33214e
Nic0ro <willarossi1@gmail.com>
2024-10-12 16:08:34 -0400

    Update fol.py

--- fol.py
+++ fol.py
@@ -5,25 +5,31 @@ df=pd.read_csv('static/info/parks.csv')
 
 m = folium.Map(location=(39.50, -98.35), zoom_start=3, tiles="cartodb positron")
 
+def iconMaker(color,icon,prefix):
+    return folium.Icon(color=color,icon=icon,prefix=prefix)
+
+def popupMaker(df,i):
+    return df.iloc[i]['name']
+
 for i in range(0,len(df)):
     if df.iloc[i]['type']=='NP':
         folium.Marker(
             location=[df.iloc[i]['lat'],df.iloc[i]['long']],#locates long and lat from the data frame and displays it
             popup=df.iloc[i]['name'], #creates a popup
-            icon=folium.Icon(color='green',icon='tree', prefix='fa')#creates a marker with this style
+            icon=iconMaker('green','tree','fa')#creates a marker with this style
         ).add_to(m)
 
     elif df.iloc[i]['type']=='NHP':
         folium.Marker(
             location=[df.iloc[i]['lat'],df.iloc[i]['long']],#locates long and lat from the data frame and displays it
             popup=df.iloc[i]['name'], #creates a popup
-            icon=folium.Icon(color='purple',icon='landmark', prefix='fa')#creates a marker with this style
+            icon=iconMaker('purple','landmark','fa')#creates a marker with this style
         ).add_to(m)
     elif df.iloc[i]['type']=='NPres':
         folium.Marker(
             location=[df.iloc[i]['lat'],df.iloc[i]['long']],#locates long and lat from the data frame and displays it
             popup=df.iloc[i]['name'], #creates a popup
-            icon=folium.Icon(color='red',icon='binoculars', prefix='fa')#creates a marker with this style
+            icon=iconMaker('red','binoculars','fa')#creates a marker with this style
         ).add_to(m)
 
 m.save("footprint.html")
